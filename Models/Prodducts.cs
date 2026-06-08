7268d22d3fde748529fa204db55c7260ac79ab1d
wach78 <dev@wach.nu>
2026-06-08 13:26:40 +0200

    added Product.cs model

---
+++ Models/Prodducts.cs
@@ -0,0 +1,18 @@
+namespace StorageApi.Models;
+
+public sealed class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Price { get; set; }
+
+    public string Category { get; set; } = string.Empty;
+
+    public string Shelf { get; set; } = string.Empty;
+
+    public int Count { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+}
